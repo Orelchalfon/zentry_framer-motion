f5c3905b9b52e31453294ae85448a78ae84a2fcd
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-01-02 23:21:42 +0200

    1.Add site logo and title 2.implement Story and Feature Components/pages !

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { domAnimation, LazyMotion } from 'framer-motion';
 import { lazy, Suspense } from "react";
 import Hero from "./Components/Hero";
 import NavBar from './Components/Navbar';
+import Features from './Components/Features';
+import Head from './Components/Head';
 const About = lazy(() => import('./Components/About'));
+const Story = lazy(() => import('./Components/Story'));
 
 const LoadingFallback = () => (
   <div className="flex-center min-h-screen w-screen bg-violet-50">
@@ -16,14 +19,19 @@ const LoadingFallback = () => (
 const App = () =>
 {
   return <>
+    <Head />
+
     <LazyMotion features={domAnimation}>
 
       <main className="relative  overflow-x-hidden">
-
         <NavBar />
         <Hero />
+
         <Suspense fallback={LoadingFallback}>
           <About />
+          <Story />
+          <Features />
+
         </Suspense>
       </main>
     </LazyMotion>
---
+++ src/Components/Story.jsx
@@ -0,0 +1,128 @@
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+import { useRef } from "react";
+
+import AnimatedTitle from "./UI/AnimatedTitle";
+import Button from "./UI/Button";
+
+const FloatingImage = () =>
+{
+  const frameRef = useRef(null);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const rotateX = useSpring(useTransform(mouseY, [-1, 1], [10, -10]), {
+    stiffness: 300,
+    damping: 30
+  });
+
+  const rotateY = useSpring(useTransform(mouseX, [-1, 1], [-10, 10]), {
+    stiffness: 300,
+    damping: 30
+  });
+
+  const handleMouseMove = (e) =>
+  {
+    const element = frameRef.current;
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const xPos = e.clientX - rect.left;
+    const yPos = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    mouseX.set((xPos - centerX) / centerX);
+    mouseY.set((yPos - centerY) / centerY);
+  };
+
+  const handleMouseLeave = () =>
+  {
+    mouseX.set(0);
+    mouseY.set(0);
+  };
+
+  return (
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <motion.img
+                  ref={frameRef}
+                  style={{
+                    rotateX,
+                    rotateY,
+                    transformPerspective: 500
+                  }}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/entrance.webp"
+                  alt="entrance.webp"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+
+            {/* for the rounded corner */}
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FloatingImage;
